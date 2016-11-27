4f5a3fcbfe2d185785ccac0c858ae35b67bf2122
SASAKURA Hideyuki <hideyuki.sasakura@gmail.com>
2016-11-27 23:36:24 +0900

    Firt sample for co-workers

---
+++ app/src/test/java/jp/co/ncsx/example/training/ExampleUnitTest.java
@@ -0,0 +1,33 @@
+package jp.co.ncsx.example.training;
+
+import android.annotation.SuppressLint;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ExampleUnitTest {
+    @Test
+    public void sortArrays() throws Exception {
+        Integer[] input = {4, 5, 7, 2, 3, 1, 9};
+
+        List<Integer> temp = Arrays.asList(input);
+        temp.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer t0, Integer t1) {
+                return t0 - t1;
+            }
+        });
+
+        assertEquals(new Integer(1), temp.get(0));
+        assertEquals(new Integer(9), temp.get(temp.size() - 1));
+    }
+
+}
